a8df319a17da1882dfb9fa4f83a54a542ae792b7
mklinkj <96992046+mklinkj@users.noreply.github.com>
2026-06-07 23:07:34 +0900

    코드 정리
    shell.nix, nix-shell 관련 코드는 제거했다.
    내가 그 환경을 쓰지 않음.

--- shell.nix
+++
@@ -1,3 +0,0 @@
-{ pkgs ? import <nixpkgs> { } }:
-with pkgs;
-mkShell { buildInputs = [ nodejs-slim-14_x yarn ]; }
--- src/extension.ts
+++ src/extension.ts
@@ -1,62 +1,61 @@
-import * as vscode from 'vscode';
-import { execSync } from 'child_process';
+import * as vscode from "vscode";
+import { execSync } from "child_process";
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.languages.registerDocumentRangeFormattingEditProvider(
-		{ scheme: 'file', language: 'java' }, {
-		provideDocumentRangeFormattingEdits(
-			document: vscode.TextDocument,
-			range: vscode.Range,
-			options: vscode.FormattingOptions,
-			token: vscode.CancellationToken): Promise<vscode.TextEdit[]> {
-			if (range.isEmpty) {
-				return Promise.resolve([]);
-			}
+  let disposable = vscode.languages.registerDocumentRangeFormattingEditProvider(
+    { scheme: "file", language: "java" },
+    {
+      provideDocumentRangeFormattingEdits(
+        document: vscode.TextDocument,
+        range: vscode.Range,
+        options: vscode.FormattingOptions,
+        token: vscode.CancellationToken,
+      ): Promise<vscode.TextEdit[]> {
+        if (range.isEmpty) {
+          return Promise.resolve([]);
+        }
 
-			return runFormatter(document.getText(range)).then(
-				stdout => {
-					return Promise.resolve([
-						vscode.TextEdit.replace(range, stdout)
-					]);
-				},
-				reason => {
-					return Promise.reject(reason);
-				}
-			);
-		}
-	});
+        return runFormatter(document.getText(range)).then(
+          (stdout) => {
+            return Promise.resolve([vscode.TextEdit.replace(range, stdout)]);
+          },
+          (reason) => {
+            return Promise.reject(reason);
+          },
+        );
+      },
+    },
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-export function deactivate() { }
+export function deactivate() {}
 
 function runFormatter(textRange: string): Promise<string> {
-	const command = (() => {
-		const executionType = `${vscode.workspace.getConfiguration('gjf').get('executionType')}`;
-		if (executionType === 'jar') {
-			const jarPath = `${vscode.workspace.getConfiguration('gjf').get('jarPath')}`;
-			const javaHome = `${vscode.workspace.getConfiguration('gjf').get('javaHome')}`;
-			const jvmOptions = `${(vscode.workspace.getConfiguration('gjf').get('jvmOptions') as string[]).join(' ')}`;
-			return `${javaHome}/bin/java -jar ${jvmOptions} ${jarPath}`;
-		} else {
-			const nativeImagePath = `${vscode.workspace.getConfiguration('gjf').get('nativeImagePath')}`;
-			return `${nativeImagePath}`;
-		}
-	})();
+  const command = (() => {
+    const executionType = `${vscode.workspace.getConfiguration("gjf").get("executionType")}`;
+    if (executionType === "jar") {
+      const jarPath = `${vscode.workspace.getConfiguration("gjf").get("jarPath")}`;
+      const javaHome = `${vscode.workspace.getConfiguration("gjf").get("javaHome")}`;
+      const jvmOptions = `${(vscode.workspace.getConfiguration("gjf").get("jvmOptions") as string[]).join(" ")}`;
+      return `${javaHome}/bin/java -jar ${jvmOptions} ${jarPath}`;
+    } else {
+      const nativeImagePath = `${vscode.workspace.getConfiguration("gjf").get("nativeImagePath")}`;
+      return `${nativeImagePath}`;
+    }
+  })();
 
-	return new Promise((resolve, reject) => {
-		try {
-			let stdout: string = execSync(
-				`${command} -`,
-				{
-					encoding: "utf8",
-					input: textRange,
-					windowsHide: true
-				});
-			resolve(stdout);
-		} catch (e) {
-			reject(e);
-		}
-	});
+  return new Promise((resolve, reject) => {
+    try {
+      let stdout: string = execSync(`${command} -`, {
+        encoding: "utf8",
+        input: textRange,
+        windowsHide: true,
+      });
+      resolve(stdout);
+    } catch (e) {
+      reject(e);
+    }
+  });
 }
